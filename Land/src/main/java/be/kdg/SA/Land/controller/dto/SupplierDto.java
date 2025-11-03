67e9dca758208676fa6eb22695f7cdaacc3f293a
RafaTobi <t.rafael.obias@gmail.com>
2025-11-03 15:44:03 +0100

    Delete Land directory

--- Land/src/main/java/be/kdg/SA/Land/controller/dto/SupplierDto.java
+++
@@ -1,4 +0,0 @@
-package be.kdg.sa.land.controller.dto;
-
-public class SupplierDto {
-}
--- Land/src/main/java/be/kdg/SA/Land/domain/enums/ResourceType.java
+++
@@ -1,9 +0,0 @@
-package be.kdg.sa.land.domain.enums;
-
-public enum ResourceType {
-    Gips,
-    IjzerErts,
-    Cement,
-    Petcoke,
-    Slak
-}
